903ff714927d8522b98d15337fe62e26f5651b72
appishiwata <app.ishiwata@gmail.com>
2023-10-27 02:27:49 +0900

    Dictionaryで例外が発生するか確認

--- Assets/Scenes/1027/Sample.cs
+++ Assets/Scenes/1027/Sample.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scenes._1027
@@ -6,7 +7,58 @@ namespace Scenes._1027
     {
         private void Start()
         {
-            Debug.Log("Hello World");
+            // Dictionaryで例外が発生するか確認
+            UserInfo userInfoWithSkills = new UserInfo
+            {
+                ID = "U1",
+                Name = "User One",
+                Level = 10,
+                UserSkills = new Skills
+                {
+                    ID = "S1",
+                    Name = "Skill One"
+                }
+            };
+
+            UserInfo userInfoWithoutSkills = new UserInfo
+            {
+                ID = "U2",
+                Name = "User Two",
+                Level = 20,
+                UserSkills = null
+            };
+
+            Dictionary<string, object> newUserInfo1 = new Dictionary<string, object>()
+            {
+                {"UserID", userInfoWithSkills.ID},
+                {"UserLevel", userInfoWithSkills.Level},
+                {"UserSkillID", userInfoWithSkills.UserSkills.ID},
+                {"UserSkillName", userInfoWithSkills.UserSkills.Name},
+            };
+
+            // しっかり例外が発生する。
+            Dictionary<string, object> newUserInfo2 = new Dictionary<string, object>()
+            {
+                {"UserID", userInfoWithoutSkills.ID},
+                {"UserLevel", userInfoWithoutSkills.Level},
+                {"UserSkillID", userInfoWithoutSkills.UserSkills.ID},
+                {"UserSkillName", userInfoWithoutSkills.UserSkills.Name},
+            };
         }
     }
+    
+    public class UserInfo
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public Skills UserSkills { get; set; }
+    }
+
+    public class Skills
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
 }
+
