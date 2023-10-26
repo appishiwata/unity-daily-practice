c305b0550901ac7730de216bc982c6979f1f8e3a
appishiwata <app.ishiwata@gmail.com>
2023-10-27 01:42:36 +0900

    シーンとスクリプトのひな型作成

---
+++ Assets/Scenes/1027/Sample.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Scenes._1027
+{
+    public class Sample : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
