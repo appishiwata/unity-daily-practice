ebb2e6618a93966a7fa00cb57b9edbdfe3ab944c
appishiwata <app.ishiwata@gmail.com>
2023-10-28 07:57:59 +0900

    dotweenをawaitで待ち受けられるように

--- Assets/Scenes/1028/Sample.cs
+++ Assets/Scenes/1028/Sample.cs
@@ -1,8 +1,25 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace Scenes._1028
 {
     public class Sample : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI _text;
+
+        async void Start()
+        {
+            var str = "Hello World1";
+            _text.text = "";
+            await _text.DOText(str, 1.0f);
+
+            await UniTask.Delay(1000);
+            
+            str = "Hello World2";
+            _text.text = "";
+            await _text.DOText(str, 1.0f);
+        }
     }
 }
