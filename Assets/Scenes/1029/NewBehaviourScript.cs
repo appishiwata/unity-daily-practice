1d33932feabe76908b35d1eeaaa16706377962dd
appishiwata <app.ishiwata@gmail.com>
2023-10-29 05:39:49 +0900

    enumを使用したプロパティの作成

--- Assets/Scenes/1029/NewBehaviourScript.cs
+++ Assets/Scenes/1029/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ namespace Scenes._1029
         void Start()
         {
             // インスタンスの作成と自己紹介の呼び出し
-            var newUser = new User("testuser", "test@example.com");
+            var newUser = new User("testuser", "test@example.com", User.UserType.Admin);
             newUser.Introduce();
         }
     }
@@ -15,21 +15,29 @@ namespace Scenes._1029
     // Userクラスの定義
     public class User
     {
+        public enum UserType {
+            Admin,
+            General,
+            Guest
+        }
+        
         // プロパティ
         private string UserName { get; set; }
         private string Email { get; set; }
+        private UserType Type { get; set; } // UserTypeを利用したプロパティ
 
         // コンストラクタ
-        public User(string userName, string email)
+        public User(string userName, string email, UserType type = UserType.General)
         {
             UserName = userName;
             Email = email;
+            Type = type;
         }
 
         // 自己紹介メソッド
         public void Introduce()
         {
-            Debug.Log($"Hello, my user name is {UserName} and my email is {Email}.");
+            Debug.Log($"Hello, my user name is {UserName} and I am a {Type} user.");
         }
     }
 }
