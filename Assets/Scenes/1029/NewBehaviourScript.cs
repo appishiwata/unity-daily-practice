6179df25df93dad28a9da5e7249d3c74fcca002c
appishiwata <app.ishiwata@gmail.com>
2023-10-29 05:24:32 +0900

    簡単なクラスの作成、インスタンス化、メソッド実行

--- Assets/Scenes/1029/NewBehaviourScript.cs
+++ Assets/Scenes/1029/NewBehaviourScript.cs
@@ -6,7 +6,30 @@ namespace Scenes._1029
     {
         void Start()
         {
-        
+            // インスタンスの作成と自己紹介の呼び出し
+            var newUser = new User("testuser", "test@example.com");
+            newUser.Introduce();
+        }
+    }
+    
+    // Userクラスの定義
+    public class User
+    {
+        // プロパティ
+        private string UserName { get; set; }
+        private string Email { get; set; }
+
+        // コンストラクタ
+        public User(string userName, string email)
+        {
+            UserName = userName;
+            Email = email;
+        }
+
+        // 自己紹介メソッド
+        public void Introduce()
+        {
+            Debug.Log($"Hello, my user name is {UserName} and my email is {Email}.");
         }
     }
 }
