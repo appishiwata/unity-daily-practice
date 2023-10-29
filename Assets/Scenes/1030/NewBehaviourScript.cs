8ee5363d9e09e1518d64787288c6315c2b6842b9
appishiwata <app.ishiwata@gmail.com>
2023-10-30 07:00:06 +0900

    UserはSkillを持っているというリレーションを定義

--- Assets/Scenes/1030/NewBehaviourScript.cs
+++ Assets/Scenes/1030/NewBehaviourScript.cs
@@ -6,7 +6,42 @@ namespace Scenes._1030
     {
         void Start()
         {
-        
+            // Userクラスのインスタンスを作成
+            var newUser = new User("testuser", "Skill One");
+            newUser.ShowUserInfo();
+        }
+    }
+    
+    public class Skill
+    {
+        public string SkillName { get; set; }
+
+        public Skill(string skillName)
+        {
+            SkillName = skillName;
+        }
+
+        public void ShowSkill()
+        {
+            Debug.Log($"Skill: {SkillName}");
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public Skill UserSkill { get; set; }
+
+        public User(string name, string skillName)
+        {
+            Name = name;
+            UserSkill = new Skill(skillName);
+        }
+
+        public void ShowUserInfo()
+        {
+            Debug.Log($"User name is {Name}");
+            UserSkill.ShowSkill();
         }
     }
 }
