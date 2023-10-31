17f6911c78a40e1b5502bb217691cf00b4fb3729
appishiwata <app.ishiwata@gmail.com>
2023-11-01 08:09:02 +0900

    スクリーンショットを撮る機能追加

--- Assets/Scenes/1101/NewBehaviourScript.cs
+++ Assets/Scenes/1101/NewBehaviourScript.cs
@@ -1,12 +1,20 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Scenes._1101
 {
     public class NewBehaviourScript : MonoBehaviour
     {
-        void Start()
+        private static readonly int ResolutionMultiplier = 1; // 解像度の倍率
+
+        [MenuItem("Tools/Screenshot")]
+        public static void Screenshot()
         {
-        
+            string filepath = "Assets/Editor/Screenshots";
+            filepath += "/screenshot_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+
+            ScreenCapture.CaptureScreenshot(filepath, ResolutionMultiplier);
+            Debug.Log("Screenshot taken at: " + filepath);
         }
     }
 }
