1a72baba8a6517153269f7c0780018ee025299e2
appishiwata <app.ishiwata@gmail.com>
2023-11-02 06:22:13 +0900

    ゲームデータ保存用のScriptableObjectを作成

--- Assets/Scenes/1102/NewBehaviourScript.cs
+++ Assets/Scenes/1102/NewBehaviourScript.cs
@@ -4,9 +4,18 @@ namespace Scenes._1102
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] GameData _gameData;
+        
         void Start()
         {
-        
+            Debug.Log($"Player Name: {_gameData.playerName}, Score: {_gameData.score}");
         }
+    }
+    
+    [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData")]
+    public class GameData : ScriptableObject
+    {
+        public int score;
+        public string playerName;
     }
 }
