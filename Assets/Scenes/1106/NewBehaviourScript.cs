0ba7ed432c9ba5fb7ffba1c3bf7ff092133406c9
appishiwata <app.ishiwata@gmail.com>
2023-11-06 08:29:58 +0900

    UserListの定義

--- Assets/Scenes/1106/NewBehaviourScript.cs
+++ Assets/Scenes/1106/NewBehaviourScript.cs
@@ -8,5 +8,21 @@ namespace Scenes._1106
         {
         
         }
+        
+        // ユーザーデータを持つクラス
+        [System.Serializable]
+        public class User
+        {
+            public int id;
+            public string name;
+            public int age;
+        }
+
+        // ScriptableObjectを継承したUserListクラス
+        [CreateAssetMenu(fileName = "UserList", menuName = "ScriptableObjects/UserList")]
+        public class UserList : ScriptableObject
+        {
+            public User[] users;
+        }
     }
 }
