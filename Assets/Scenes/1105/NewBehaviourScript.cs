e6746161a63750927cac9a692baa0a129df84578
appishiwata <app.ishiwata@gmail.com>
2023-11-05 11:28:04 +0900

    ユーザー一覧のScriptableObjectを作成して読み込み

--- Assets/Scenes/1105/NewBehaviourScript.cs
+++ Assets/Scenes/1105/NewBehaviourScript.cs
@@ -4,9 +4,27 @@ namespace Scenes._1105
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        public UserList _userList;
+        
         void Start()
         {
+            foreach (var user in _userList.users)
+            {
+                Debug.Log($"User Name: {user.name}, User Age: {user.age}");
+            }
+        }
         
+        // ユーザーデータを持つクラス
+        [System.Serializable]
+        public class User {
+            public string name;
+            public int age;
+        }
+
+        // ScriptableObjectを継承したUserListクラス
+        [CreateAssetMenu(fileName = "UserList", menuName = "ScriptableObjects/UserList")]
+        public class UserList : ScriptableObject {
+            public User[] users;
         }
     }
 }
