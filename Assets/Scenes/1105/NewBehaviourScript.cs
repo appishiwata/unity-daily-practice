2c1f27e374666267174385be0bcbfd77b615c51f
appishiwata <app.ishiwata@gmail.com>
2023-11-05 11:35:03 +0900

    ScriptableObjectをscriptから呼び出す場合(Resources意外から)、エディター上でのみ動作

--- Assets/Scenes/1105/NewBehaviourScript.cs
+++ Assets/Scenes/1105/NewBehaviourScript.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Scenes._1105
@@ -12,6 +13,16 @@ namespace Scenes._1105
             {
                 Debug.Log($"User Name: {user.name}, User Age: {user.age}");
             }
+            
+            // Resourcesフォルダ内意外の場所にあるScriptableObjectをスクリプトから読み込む
+            // Editor上でのみ動作するコード(ビルド時にエラーになるのでビルドするならif文で囲う)
+#if UNITY_EDITOR
+            var userList = AssetDatabase.LoadAssetAtPath<UserList>("Assets/Scenes/1105/UserList.asset");
+            foreach (var user in userList.users)
+            {
+                Debug.Log($"User Name: {user.name}, User Age: {user.age}");
+            }
+#endif
         }
         
         // ユーザーデータを持つクラス
