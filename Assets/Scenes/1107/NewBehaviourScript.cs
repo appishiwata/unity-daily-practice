fd124f53065b8068452665408406c01a3a29b31b
appishiwata <app.ishiwata@gmail.com>
2023-11-07 10:25:51 +0900

    簡単なシングルトンの使い方

--- Assets/Scenes/1107/NewBehaviourScript.cs
+++ Assets/Scenes/1107/NewBehaviourScript.cs
@@ -6,7 +6,24 @@ namespace Scenes._1107
     {
         void Start()
         {
-        
+            User.Instance.Id = 1;
+            User.Instance.Name = "test";
+            User.Instance.Age = 20;
+            User.Instance.DebugLog();
+        }
+    }
+    
+    public class User
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+
+        public static User Instance { get; } = new();
+
+        public void DebugLog()
+        {
+            Debug.Log($"User: {Id}, {Name}, {Age}");
         }
     }
 }
