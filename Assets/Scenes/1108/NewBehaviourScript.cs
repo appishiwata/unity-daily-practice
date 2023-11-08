69e99850c89a0d092a6add8f3025a7be92a50b7b
appishiwata <app.ishiwata@gmail.com>
2023-11-08 23:49:52 +0900

    エンドロールもどきの作成

--- Assets/Scenes/1108/NewBehaviourScript.cs
+++ Assets/Scenes/1108/NewBehaviourScript.cs
@@ -1,12 +1,16 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Scenes._1108
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] GameObject _move;
+        
         void Start()
         {
-        
+            _move.transform.DOMoveY(1000, 10f)
+                .SetRelative();
         }
     }
 }
