86186637537d665fba868f5546840ec2a15bfa84
appishiwata <app.ishiwata@gmail.com>
2023-11-09 10:05:08 +0900

    エンドロールもどき scrollviewの方が簡単

--- Assets/Scenes/1109/NewBehaviourScript.cs
+++ Assets/Scenes/1109/NewBehaviourScript.cs
@@ -1,12 +1,21 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._1109
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] ScrollRect _scrollRect;
+        
         void Start()
         {
-        
+            _scrollRect.normalizedPosition = new Vector2(0, 0f);
+
+            DOVirtual.Float(0f, 1f, 10f, value =>
+            {
+                _scrollRect.normalizedPosition = new Vector2(0f, value);
+            });
         }
     }
 }
