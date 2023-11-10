4caf22d9c43940bcc8e0a23d97f682a441b01ac9
appishiwata <app.ishiwata@gmail.com>
2023-11-10 22:55:38 +0900

    2秒待つメソッドを待機して実行

--- Assets/Scenes/1110/NewBehaviourScript.cs
+++ Assets/Scenes/1110/NewBehaviourScript.cs
@@ -1,12 +1,20 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Scenes._1110
 {
     public class NewBehaviourScript : MonoBehaviour
     {
-        void Start()
+        async void Start()
         {
-        
+            await WaitForTwoSecondsAsync();
+            
+            Debug.Log("Start");
+        }
+
+        private async Task WaitForTwoSecondsAsync()
+        {
+            await Task.Delay(2000);
         }
     }
 }
