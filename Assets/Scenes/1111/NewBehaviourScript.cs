b16a27f5ebe2b40c32f2843e88d4f871fec387ea
appishiwata <app.ishiwata@gmail.com>
2023-11-11 10:17:56 +0900

    テスト用APIにリクエスト ステータスを確認

--- Assets/Scenes/1111/NewBehaviourScript.cs
+++ Assets/Scenes/1111/NewBehaviourScript.cs
@@ -1,12 +1,37 @@
+using System.Net.Http;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Scenes._1111
 {
     public class NewBehaviourScript : MonoBehaviour
     {
-        void Start()
+        async void Start()
         {
+            Debug.Log("Start");
+            
+            await RequestHttp();
+            
+            Debug.Log("End");
+        }
         
+        private static async Task RequestHttp()
+        {
+            var client = new HttpClient();
+       
+            try
+            {
+                var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+                response.EnsureSuccessStatusCode();
+                await response.Content.ReadAsStringAsync();
+                Debug.Log("Status Code: " + response.StatusCode);
+            }
+            catch(HttpRequestException)
+            {
+                Debug.Log("\nException Caught!");
+            }
+
+            client.Dispose();
         }
     }
 }
