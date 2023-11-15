80b143573e28b57990a260b5080ee3cd6a41fdd7
appishiwata <app.ishiwata@gmail.com>
2023-11-15 21:00:37 +0900

    addressableを使用、画像の表示色々

--- Assets/Scenes/1115/NewBehaviourScript.cs
+++ Assets/Scenes/1115/NewBehaviourScript.cs
@@ -1,12 +1,27 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
 
 namespace Scenes._1115
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Image _imageSprite;
+        [SerializeField] Image _imageResources;
+        [SerializeField] Image _imageAddressable;
+        [SerializeField] Sprite _sprite;
+        
         void Start()
         {
-        
+            // 普通にspriteを指定
+            _imageSprite.sprite = _sprite;
+
+            // Resources.Loadでspriteを指定 (Unity Editor上でのみ動作)
+            _imageResources.sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Scenes/1115/sample.png");
+
+            // addressableでspriteを指定
+            _imageAddressable.sprite = Addressables.LoadAssetAsync<Sprite>("Assets/Scenes/1115/sample.png").WaitForCompletion();
         }
     }
 }
