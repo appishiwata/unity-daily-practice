5dd2f156fba504e4be565bf7cf4871cf911cf757
appishiwata <app.ishiwata@gmail.com>
2023-11-16 23:46:44 +0900

    Unitaskで待機

--- Assets/Scenes/1116/NewBehaviourScript.cs
+++ Assets/Scenes/1116/NewBehaviourScript.cs
@@ -1,12 +1,23 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Scenes._1116
 {
     public class NewBehaviourScript : MonoBehaviour
     {
-        void Start()
+        async void Start()
         {
+            await RunAsync();
+        }
         
+        private async UniTask RunAsync()
+        {
+            Debug.Log("Start");
+
+            // 時間をかける処理
+            await UniTask.Delay(1000);
+
+            Debug.Log("End");
         }
     }
 }
