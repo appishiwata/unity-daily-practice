d0cdefa20c76b1b73d612caeee9e847182875c02
appishiwata <app.ishiwata@gmail.com>
2023-11-16 23:51:21 +0900

    Unirxで長押し検知

--- Assets/Scenes/1116/NewBehaviourScript.cs
+++ Assets/Scenes/1116/NewBehaviourScript.cs
@@ -1,13 +1,42 @@
 using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
 
 namespace Scenes._1116
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        private readonly float LongPressTime = 2.0f;
+        private float pressTime = 0;
+        
         async void Start()
         {
             await RunAsync();
+            
+            var isPressed = false;
+
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButton(0))
+                .Subscribe(_ => {
+                    isPressed = true;
+                    pressTime += Time.deltaTime;
+                
+                    if (pressTime >= LongPressTime)
+                    {
+                        Debug.Log("Long Press Detected!");
+                        pressTime = 0;
+                    }
+                });
+        
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButtonUp(0))
+                .Subscribe(_ => {
+                    if (isPressed)
+                    {
+                        isPressed = false;
+                        pressTime = 0;
+                    }
+                });
         }
         
         private async UniTask RunAsync()
