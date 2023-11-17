e44729e042c1f265f19057b220c949365c22b188
appishiwata <app.ishiwata@gmail.com>
2023-11-17 20:14:55 +0900

    普通の直線移動

--- Assets/Scenes/1117/NewBehaviourScript.cs
+++ Assets/Scenes/1117/NewBehaviourScript.cs
@@ -1,12 +1,23 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._1117
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Image _image;
+        [SerializeField] Image _target;
+        [SerializeField] Image _middle;
+        
         void Start()
         {
-        
+            var startPoint = _image.transform.position;
+            var middlePoint = _middle.transform.position;
+            var endPoint = _target.transform.position;
+            
+            // 通常の移動
+            _image.transform.DOMove(endPoint, 2);
         }
     }
 }
