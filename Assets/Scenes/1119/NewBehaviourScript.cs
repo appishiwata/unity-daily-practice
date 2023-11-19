9c2060a1671545b1140e30f1bbc84041e537b127
appishiwata <app.ishiwata@gmail.com>
2023-11-19 16:56:36 +0900

    jsonのシリアライズとデシリアライズ

--- Assets/Scenes/1119/NewBehaviourScript.cs
+++ Assets/Scenes/1119/NewBehaviourScript.cs
@@ -6,7 +6,29 @@ namespace Scenes._1119
     {
         void Start()
         {
+            // 新しいPlayerInfoオブジェクトを作成
+            var player = new PlayerInfo
+            {
+                name = "John Doe",
+                level = 10,
+                health = 100.0f
+            };
+
+            // オブジェクトをJSON文字列にシリアライズする
+            var json = JsonUtility.ToJson(player);
+            Debug.Log(json); // {"name":"John Doe","level":10,"health":100.0}
+
+            // JSON文字列をPlayerInfoオブジェクトにデシリアライズする
+            var deserializedPlayer = JsonUtility.FromJson<PlayerInfo>(json);
+            Debug.Log($"Name: {deserializedPlayer.name}, Level: {deserializedPlayer.level}, Health: {deserializedPlayer.health}");
+        }
         
+        [System.Serializable]
+        public class PlayerInfo
+        {
+            public string name;
+            public int level;
+            public float health;
         }
     }
 }
