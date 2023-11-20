bb5c85994fd52f13802b8a6f63f149345a6bbc66
appishiwata <app.ishiwata@gmail.com>
2023-11-20 23:47:39 +0900

    オブサーバー追加、通知の発行

--- Assets/Scenes/1120/NewBehaviourScript.cs
+++ Assets/Scenes/1120/NewBehaviourScript.cs
@@ -1,3 +1,4 @@
+using UniRx;
 using UnityEngine;
 
 namespace Scenes._1120
@@ -6,7 +7,16 @@ namespace Scenes._1120
     {
         void Start()
         {
-        
+            // Subjectの初期化
+            var subject = new Subject<string>();
+
+            // オブザーバーの追加
+            subject.Subscribe(x => Debug.Log($"Observer 1 received: {x}"));
+            subject.Subscribe(x => Debug.Log($"Observer 2 received: {x}"));
+
+            // 通知値の発行
+            subject.OnNext("Hello");
+            subject.OnNext("World");
         }
     }
 }
