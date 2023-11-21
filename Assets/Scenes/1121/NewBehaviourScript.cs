e40f564932627b10484d8261e14b8967d34b276c
appishiwata <app.ishiwata@gmail.com>
2023-11-21 12:56:55 +0900

    Subject<int>

--- Assets/Scenes/1121/NewBehaviourScript.cs
+++ Assets/Scenes/1121/NewBehaviourScript.cs
@@ -7,8 +7,11 @@ namespace Scenes._1121
     public class NewBehaviourScript : MonoBehaviour
     {
         // Subjectのインスタンスを作成
-        private Subject<Unit> _buttonOnClickSubject = new Subject<Unit>();
-
+        private Subject<Unit> _buttonOnClickSubject = new();
+        private Subject<int> _clickCountSubject = new();
+        private int _clickCount;
+        private int _clickCountUniRx;
+        
         // Buttonの参照
         public Button _exampleButton;
         
@@ -16,17 +19,27 @@ namespace Scenes._1121
         {
             // ボタンクリック時にSubjectに通知
             _exampleButton.onClick.AddListener(() => { _buttonOnClickSubject.OnNext(Unit.Default); });
+            _exampleButton.onClick.AddListener(() => { _clickCount++; _clickCountSubject.OnNext(_clickCount); });
 
             // Subjectの購読
             _buttonOnClickSubject.Subscribe(_ =>
             {
                 Debug.Log("Button was clicked!");
             });
+            _clickCountSubject.Subscribe(clickCount =>
+            {
+                Debug.Log("Button was clicked " + clickCount + " times!");
+            });
             
             // UniRxでの書き方
             _exampleButton.OnClickAsObservable().Subscribe(_ =>
             {
                 Debug.Log("Button was clicked! (UniRx)");
+            });
+            _exampleButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                _clickCountUniRx++;
+                Debug.Log("Button was clicked " + _clickCountUniRx + " times! (UniRx)");
             });
         }
     }
