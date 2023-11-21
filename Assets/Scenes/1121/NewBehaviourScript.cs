4210c6118198a505ddf3036253b9df9768752873
appishiwata <app.ishiwata@gmail.com>
2023-11-21 12:49:20 +0900

    Subject<Unit> 型

--- Assets/Scenes/1121/NewBehaviourScript.cs
+++ Assets/Scenes/1121/NewBehaviourScript.cs
@@ -1,12 +1,27 @@
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._1121
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        // Subjectのインスタンスを作成
+        private Subject<Unit> _buttonOnClickSubject = new Subject<Unit>();
+
+        // Buttonの参照
+        public Button _exampleButton;
+        
         void Start()
         {
-        
+            // ボタンクリック時にSubjectに通知
+            _exampleButton.onClick.AddListener(() => { _buttonOnClickSubject.OnNext(Unit.Default); });
+
+            // Subjectの購読
+            _buttonOnClickSubject.Subscribe(_ =>
+            {
+                Debug.Log("Button was clicked!");
+            });
         }
     }
 }
