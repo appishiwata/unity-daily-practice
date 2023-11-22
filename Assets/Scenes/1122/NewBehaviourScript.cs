dcf99e5f82c46b6807ae6b767eddda191fc863d7
appishiwata <app.ishiwata@gmail.com>
2023-11-22 23:55:05 +0900

    subject型 string OnNext

--- Assets/Scenes/1122/NewBehaviourScript.cs
+++ Assets/Scenes/1122/NewBehaviourScript.cs
@@ -1,12 +1,21 @@
+using UniRx;
 using UnityEngine;
 
 namespace Scenes._1122
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        private Subject<string> _clickSubject = new();
+        
         void Start()
         {
-        
+            _clickSubject.Subscribe(str =>
+            {
+                Debug.Log(str);
+            });
+            
+            _clickSubject.OnNext("Message that will be ignored");
+            _clickSubject.OnNext("Message that will be ignored 2");
         }
     }
 }
