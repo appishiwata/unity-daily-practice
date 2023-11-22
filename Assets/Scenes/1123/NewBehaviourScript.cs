5b936dc52110c54d36d2f6f9d29d28dc92b39edb
appishiwata <app.ishiwata@gmail.com>
2023-11-23 07:34:35 +0900

    変数にアクセス

--- Assets/Scenes/1123/NewBehaviourScript.cs
+++ Assets/Scenes/1123/NewBehaviourScript.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._1123
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Image _image;
+        
         void Start()
         {
-        
+            // 変数
+            _image.color = Color.red;
+            _image.raycastTarget = false;
+            _image.maskable = false;
+            _image.canvasRenderer.SetAlpha(0.5f);
         }
     }
 }
