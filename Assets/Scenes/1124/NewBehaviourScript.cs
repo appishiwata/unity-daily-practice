9e5158a40b678b8557ffc05716a7acd8daaf7dbb
appishiwata <app.ishiwata@gmail.com>
2023-11-24 08:10:20 +0900

    関数にアクセス

--- Assets/Scenes/1124/NewBehaviourScript.cs
+++ Assets/Scenes/1124/NewBehaviourScript.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._1124
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Image _image;
+        
         void Start()
         {
-        
+            _image.type = Image.Type.Filled;
+            _image.fillAmount = 0.25f;
+            _image.fillMethod = Image.FillMethod.Radial360;
+            _image.SetNativeSize();
         }
     }
 }
