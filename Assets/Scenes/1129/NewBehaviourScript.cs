21ff78373dcdfaa88f68716c0444159471e540a4
appishiwata <app.ishiwata@gmail.com>
2023-11-29 08:16:03 +0900

    メソッド

--- Assets/Scenes/1129/NewBehaviourScript.cs
+++ Assets/Scenes/1129/NewBehaviourScript.cs
@@ -6,7 +6,34 @@ namespace Scenes._1129
     {
         void Start()
         {
+            SampleMethod();
+            SampleMethodWithArg(10);
+            Debug.Log(SampleMethodWithReturn());
+            Debug.Log(SampleMethodWithArgAndReturn(10));
+        }
         
+        // サンプルのメソッド
+        public void SampleMethod()
+        {
+            Debug.Log("SampleMethod");
+        }
+        
+        // 引数ありのサンプルのメソッド
+        public void SampleMethodWithArg(int arg)
+        {
+            Debug.Log("SampleMethodWithArg: " + arg);
+        }
+        
+        // 戻り値ありのサンプルのメソッド
+        public int SampleMethodWithReturn()
+        {
+            return 100;
+        }
+        
+        // 引数あり、戻り値ありのサンプルのメソッド
+        public int SampleMethodWithArgAndReturn(int arg)
+        {
+            return arg * 2;
         }
     }
 }
