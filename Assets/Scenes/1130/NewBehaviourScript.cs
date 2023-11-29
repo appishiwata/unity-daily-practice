67678e7f5e85b81fedbc26b5e23afa17484b1a3d
appishiwata <app.ishiwata@gmail.com>
2023-11-30 08:25:20 +0900

    クラス

--- Assets/Scenes/1130/NewBehaviourScript.cs
+++ Assets/Scenes/1130/NewBehaviourScript.cs
@@ -6,7 +6,33 @@ namespace Scenes._1130
     {
         void Start()
         {
+            Employee employee = new Employee("Taro", "Yamada", "Sales");
+            employee.PrintName();
+            employee.PrintDepartment();
+        }
+    }
+    
+    public class Employee
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Department { get; set; }
+    
+        public Employee(string firstName, string lastName, string department)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+        }
         
+        public void PrintName()
+        {
+            Debug.Log($"Employee Name: {FirstName} {LastName}");
+        }
+    
+        public void PrintDepartment()
+        {
+            Debug.Log($"Department: {Department}");
         }
     }
 }
