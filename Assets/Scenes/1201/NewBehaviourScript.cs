c726684bef4046763f6c993b18860d8fe6be0350
appishiwata <app.ishiwata@gmail.com>
2023-12-01 08:20:12 +0900

    GameObject

--- Assets/Scenes/1201/NewBehaviourScript.cs
+++ Assets/Scenes/1201/NewBehaviourScript.cs
@@ -4,9 +4,20 @@ namespace Scenes._1201
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] GameObject _gameObject;
+        
         void Start()
         {
-        
+            // GameObjectクラスの変数
+            Debug.LogFormat("GameObject: {0}", _gameObject.scene.name);
+            Debug.LogFormat("tag: {0}, layer: {1}", _gameObject.tag, _gameObject.layer);
+            Debug.LogFormat("activeSelf: {0}, activeInHierarchy: {1}", _gameObject.activeSelf, _gameObject.activeInHierarchy);
+            
+            // GameObjectクラスの関数
+            _gameObject.SetActive(false);
+
+            // 確認
+            Debug.LogFormat("activeSelf: {0}, activeInHierarchy: {1}", _gameObject.activeSelf, _gameObject.activeInHierarchy);
         }
     }
 }
