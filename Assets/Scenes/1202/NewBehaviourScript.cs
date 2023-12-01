9c23874fe1906627e83f554a4d191e8603b2670b
appishiwata <app.ishiwata@gmail.com>
2023-12-02 08:54:03 +0900

    GameObject 関数

--- Assets/Scenes/1202/NewBehaviourScript.cs
+++ Assets/Scenes/1202/NewBehaviourScript.cs
@@ -4,9 +4,39 @@ namespace Scenes._1202
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] GameObject _gameObject;
+        
         void Start()
         {
-        
+            // =====================================
+            // GameObjectのPublic関数
+            // =====================================
+
+            // AddComponent関数
+            _gameObject.AddComponent<Rigidbody>();
+            
+            // GetComponent関数
+            var rigidbody = _gameObject.GetComponent<Rigidbody>();
+            
+            Debug.LogFormat("rigidbody: {0}", rigidbody);
+            
+            // TryGetComponent関数
+            if (_gameObject.TryGetComponent<Rigidbody>(out var rigidbodyTry))
+            {
+                Debug.LogFormat("rigidbodyTry: {0}", rigidbodyTry);
+            }
+            else
+            {
+                Debug.Log("rigidbodyTry: null");
+            }
+            
+            // =====================================
+            // GameObjectのStatic関数
+            // =====================================
+            
+            // Find関数
+            var gameObject = GameObject.Find("GameObject");
+            Debug.LogFormat("gameObject: {0}", gameObject);
         }
     }
 }
