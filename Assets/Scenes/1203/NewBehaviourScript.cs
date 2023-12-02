b680f5d295f594a1edec729f6ce3c72645875964
appishiwata <app.ishiwata@gmail.com>
2023-12-03 07:45:20 +0900

    MonoBehaviour

--- Assets/Scenes/1203/NewBehaviourScript.cs
+++ Assets/Scenes/1203/NewBehaviourScript.cs
@@ -4,9 +4,56 @@ namespace Scenes._1203
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        // MonoBehaviourクラスの関数
+        
         void Start()
         {
+            Debug.Log("Game started!");
+        }
+
+        void Update()
+        {
+            //Debug.Log("Frame updated!");
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("Collision detected!");
+        }
         
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Trigger detected!");
+        }
+        
+        void OnMouseDown()
+        {
+            Debug.Log("Mouse clicked!");
+        }
+        
+        void OnMouseEnter()
+        {
+            Debug.Log("Mouse entered!");
+        }
+        
+        void OnMouseExit()
+        {
+            Debug.Log("Mouse exited!");
+        }
+        
+        void OnMouseOver()
+        {
+            Debug.Log("Mouse overed!");
+        }
+        
+        void OnMouseUp()
+        {
+            Debug.Log("Mouse released!");
+        }
+        
+        void OnMouseUpAsButton()
+        {
+            Debug.Log("Mouse released as button!");
         }
     }
 }
