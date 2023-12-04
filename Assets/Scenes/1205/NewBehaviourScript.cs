50b897b928db46232345ae9afe042b7b478c54fe
appishiwata <app.ishiwata@gmail.com>
2023-12-05 07:24:14 +0900

    Object

--- Assets/Scenes/1205/NewBehaviourScript.cs
+++ Assets/Scenes/1205/NewBehaviourScript.cs
@@ -1,12 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Scenes._1205
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Object _obj;
+        
         void Start()
         {
-        
+            Debug.Log(_obj.name);
+            _obj.GameObject().SetActive(false);
+            
+            // Objectクラス : Unity が参照できるすべてのオブジェクトの基本クラス
         }
     }
 }
