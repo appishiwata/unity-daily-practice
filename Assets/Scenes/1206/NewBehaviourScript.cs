92150a931982edfc2a7ad5ad47574a52686d2b57
appishiwata <app.ishiwata@gmail.com>
2023-12-06 07:19:03 +0900

    Transform

--- Assets/Scenes/1206/NewBehaviourScript.cs
+++ Assets/Scenes/1206/NewBehaviourScript.cs
@@ -4,9 +4,15 @@ namespace Scenes._1206
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Transform _transform;
+        
         void Start()
         {
-        
+            // Transoformクラス : ゲームオブジェクトの位置、回転、スケールを管理するクラス
+            
+            _transform.position = new Vector3(1, 2, 3);
+            _transform.rotation = Quaternion.Euler(0, 90, 0);
+            _transform.localScale = new Vector3(2, 2, 2);
         }
     }
 }
