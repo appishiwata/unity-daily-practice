12f0147092496ceb0837d56bbddcdaa13c7ff222
appishiwata <app.ishiwata@gmail.com>
2023-12-10 07:13:55 +0900

    ScriptableObject

--- Assets/Scenes/1210/NewBehaviourScript.cs
+++ Assets/Scenes/1210/NewBehaviourScript.cs
@@ -4,9 +4,23 @@ namespace Scenes._1210
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        public Enemy enemyData;
+        
         void Start()
         {
-        
+            // ScriptableObjectクラス : シーンをまたいでデータを保持するクラス
+
+            Debug.Log($"Enemy Name: {enemyData.enemyName}");
+            Debug.Log($"Hit Points: {enemyData.hitPoints}");
+            Debug.Log($"Speed: {enemyData.speed}");
         }
+    }
+    
+    [CreateAssetMenu(fileName = "New Enemy", menuName = "ScriptableObjects/Enemy")]
+    public class Enemy : ScriptableObject
+    {
+        public string enemyName;
+        public int hitPoints;
+        public float speed;
     }
 }
