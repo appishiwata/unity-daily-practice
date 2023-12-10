a4466bdc7e5894eb5b19d0744e4dccea9449879e
appishiwata <app.ishiwata@gmail.com>
2023-12-11 06:43:04 +0900

    Time

--- Assets/Scenes/1211/NewBehaviourScript.cs
+++ Assets/Scenes/1211/NewBehaviourScript.cs
@@ -6,7 +6,25 @@ namespace Scenes._1211
     {
         void Start()
         {
-        
+            // Timeクラス : 時間に関する情報を提供するクラス
+            // サンプル
+            // deltaTime : 前フレームからの経過時間
+            Debug.Log(Time.deltaTime);
+            
+            // fixedDeltaTime : 固定フレームレートの場合の経過時間
+            Debug.Log(Time.fixedDeltaTime);
+            
+            // fixedTime : 固定フレームレートの場合の経過時間
+            Debug.Log(Time.fixedTime);
+            
+            // frameCount : 現在のフレーム数
+            Debug.Log(Time.frameCount);
+            
+            // time : ゲーム開始からの経過時間
+            Debug.Log(Time.time);
+            
+            // timeScale : ゲームの時間の進行速度
+            Debug.Log(Time.timeScale);
         }
     }
 }
