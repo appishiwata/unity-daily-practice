990372aa8c9a8d73a42d85bf97dd2e9118d7a087
appishiwata <app.ishiwata@gmail.com>
2023-12-13 07:13:44 +0900

    Random

--- Assets/Scenes/1213/NewBehaviourScript.cs
+++ Assets/Scenes/1213/NewBehaviourScript.cs
@@ -6,7 +6,21 @@ namespace Scenes._1213
     {
         void Start()
         {
-        
+            // Rondomクラス : 乱数を生成するクラス
+            // サンプル
+            // Range : 指定した範囲の乱数を生成する
+            Debug.Log(Random.Range(0, 10));
+            
+            // Randomのメソッド
+            // InitState : 乱数のシードを設定する
+            Random.InitState(100);
+            
+            // Randomの変数
+            // state : 乱数の状態を取得する
+            Debug.Log(Random.state);
+            
+            // Rotation : 乱数の状態を取得する
+            Debug.Log(Random.rotation);
         }
     }
 }
