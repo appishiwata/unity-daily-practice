5a47b8dbb2ab6a3a77cc08cfab4155934eb0d940
appishiwata <app.ishiwata@gmail.com>
2023-12-25 08:17:32 +0900

    Text - TextMeshPro

--- Assets/Scenes/2023/1225/NewBehaviourScript.cs
+++ Assets/Scenes/2023/1225/NewBehaviourScript.cs
@@ -1,13 +1,34 @@
+using TMPro;
 using UnityEngine;
 
 namespace Scenes._2023._1225
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] TextMeshProUGUI _text;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            _text.text = "Hello, World!";
+            
+            // テキストのフォントスタイルを変更
+            _text.fontStyle = FontStyles.Bold;
+            
+            // テキストのアウトラインを有効にする
+            _text.enableAutoSizing = true;
+            
+            // テキストのアウトラインの色を変更
+            _text.outlineColor = Color.red;
+            
+            // テキストのアウトラインの幅を変更
+            _text.outlineWidth = 0.2f;
+            
+            // 真ん中に揃える
+            _text.alignment = TextAlignmentOptions.Center;
+            
+            // サイズを変更
+            _text.fontSize = 30;
         }
 
         // Update is called once per frame
