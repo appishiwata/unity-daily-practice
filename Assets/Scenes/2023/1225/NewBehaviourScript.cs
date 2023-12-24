ddd970cde57662a009e35a2a48a727e25fe17424
appishiwata <app.ishiwata@gmail.com>
2023-12-25 08:12:32 +0900

    UIパーツのサンプル

---
+++ Assets/Scenes/2023/1225/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Scenes._2023._1225
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
