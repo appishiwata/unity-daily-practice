f8af00954f5fc05e8f8c594f896190dc2d11b04e
appishiwata <app.ishiwata@gmail.com>
2024-01-05 08:34:10 +0900

    Panel

--- Assets/Scenes/2024/0105/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0105/NewBehaviourScript.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0105
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Image _panel;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // Panelの色を変更
+            _panel.color = new Color(1, 0, 0, 1);
+            
+            // Panelのアルファ値を変更
+            _panel.color = new Color(1, 0, 0, 0.5f);
+            
+            // PanelはImageコンポーネントを持っていて、RectTransformがストレッチされている
         }
 
         // Update is called once per frame
