653eaafa3be165a5f8b11be0726537a820c78c83
appishiwata <app.ishiwata@gmail.com>
2024-01-04 09:14:23 +0900

    Raw Image

--- Assets/Scenes/2024/0104/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0104/NewBehaviourScript.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0104
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] RawImage _rawImage;
+        [SerializeField] Texture _texture;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // RawImageにテクスチャを設定
+            _rawImage.texture = _texture;
+            
+            // RawImageのサイズを変更
+            _rawImage.rectTransform.sizeDelta = new Vector2(200, 200);
+            
+            // RawImageとImageの違い
+            // RawImageはテクスチャを直接表示するためのコンポーネント
+            // Imageはスプライトを表示するためのコンポーネント
         }
 
         // Update is called once per frame
