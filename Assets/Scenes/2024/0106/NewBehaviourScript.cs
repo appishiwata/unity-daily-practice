6684ced6a7b064a5806c6e9b1e396de86d35ae08
appishiwata <app.ishiwata@gmail.com>
2024-01-06 09:32:11 +0900

    Toggle

--- Assets/Scenes/2024/0106/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0106/NewBehaviourScript.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0106
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Toggle _toggle;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // Toggleの状態を取得
+            Debug.Log(_toggle.isOn);
+            
+            // Toggleの状態を変更
+            _toggle.isOn = false;
+            
+            // Toggleの状態を変更
+            Debug.Log(_toggle.isOn);
         }
 
         // Update is called once per frame
