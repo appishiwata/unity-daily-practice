2b2ffc2c798b7debe90928f0128f4af2378e6a09
appishiwata <app.ishiwata@gmail.com>
2024-01-08 08:54:12 +0900

    Scrollbar

--- Assets/Scenes/2024/0108/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0108/NewBehaviourScript.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0108
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Scrollbar _scrollbar;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // Scrollbarの値を取得
+            Debug.Log(_scrollbar.value);
+            
+            // Scrollbarの値を変更
+            _scrollbar.value = 0.5f;
+            
+            // Scrollbarの値を変更
+            Debug.Log(_scrollbar.value);
         }
 
         // Update is called once per frame
