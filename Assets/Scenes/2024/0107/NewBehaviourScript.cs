c833fc6010a1fc5a0dd377ce06751d9bb775311f
appishiwata <app.ishiwata@gmail.com>
2024-01-07 09:14:03 +0900

    Slider

--- Assets/Scenes/2024/0107/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0107/NewBehaviourScript.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0107
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Slider _slider;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // Sliderの値を取得
+            Debug.Log(_slider.value);
+            
+            // Sliderの値を変更
+            _slider.value = 0.5f;
+            
+            // Sliderの値を変更
+            Debug.Log(_slider.value);
         }
 
         // Update is called once per frame
