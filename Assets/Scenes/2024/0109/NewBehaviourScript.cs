a3f67e76b10eaa3cc18b7668e65192e96f771fec
appishiwata <app.ishiwata@gmail.com>
2024-01-09 08:26:03 +0900

    Scroll View

--- Assets/Scenes/2024/0109/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0109/NewBehaviourScript.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0109
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] ScrollRect _scrollView;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // ScrollRectのスクロール範囲を取得
+            Debug.Log(_scrollView.content.rect);
+            
+            // ScrollRectのスクロール範囲を変更
+            _scrollView.content.sizeDelta = new Vector2(1000, 1000);
+            
+            // ScrollRectのスクロール範囲を変更
+            Debug.Log(_scrollView.content.rect);
         }
 
         // Update is called once per frame
