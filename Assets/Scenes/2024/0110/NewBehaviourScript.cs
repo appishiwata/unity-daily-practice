1566274823842e03d66150d9d3d3a45922d85050
appishiwata <app.ishiwata@gmail.com>
2024-01-10 09:21:12 +0900

    Button - TextMeshPro

--- Assets/Scenes/2024/0110/NewBehaviourScript.cs
+++ Assets/Scenes/2024/0110/NewBehaviourScript.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes._2024._0110
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] Button _button;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            // Buttonのクリックイベントを追加
+            _button.onClick.AddListener(() =>
+            {
+                Debug.Log("Button Clicked");
+            });
+            
+            _button.interactable = false;
+            Debug.Log(_button.interactable);
+
+            _button.interactable = true;
+            Debug.Log(_button.interactable);
         }
 
         // Update is called once per frame
