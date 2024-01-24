a780d21fd1a6d0d75737ce4cdc14db744f102f49
appishiwata <app.ishiwata@gmail.com>
2024-01-24 09:03:21 +0900

    Popup Dialog 閉じるボタン

--- Assets/Scenes/Template/02_PopupDialog/PopupDialog.cs
+++ Assets/Scenes/Template/02_PopupDialog/PopupDialog.cs
@@ -1,5 +1,7 @@
 using TMPro;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes.Template._02_PopupDialog
 {
@@ -7,6 +9,15 @@ namespace Scenes.Template._02_PopupDialog
     {
         [SerializeField] GameObject _popupDialog;
         [SerializeField] TextMeshProUGUI _messageText;
+        [SerializeField] Button _closeButton;
+        
+        void Start()
+        {
+            _closeButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                _popupDialog.SetActive(false);
+            }).AddTo(this);
+        }
         
         public void Show(string message)
         {
