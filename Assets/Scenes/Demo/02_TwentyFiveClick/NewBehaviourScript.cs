7dbc27d6c48a5245d60f8216466e237dcd435286
appishiwata <app.ishiwata@gmail.com>
2024-01-29 10:12:34 +0900

    25早押しゲーム Prefabから数字ボタン生成

--- Assets/Scenes/Demo/02_TwentyFiveClick/NewBehaviourScript.cs
+++ Assets/Scenes/Demo/02_TwentyFiveClick/NewBehaviourScript.cs
@@ -4,8 +4,16 @@ namespace Scenes.Demo._02_TwentyFiveClick
 {
     public class NewBehaviourScript : MonoBehaviour
     {
+        [SerializeField] GameObject _numberButtonPrefab;
+        [SerializeField] Transform _content;
+        
         void Start()
         {
+            for (var i = 1; i <= 25; i++)
+            {
+                var numberButton = Instantiate(_numberButtonPrefab, _content).GetComponent<NumberButton>();
+                numberButton.SetNumber(i);
+            }
         }
     }
 }
--- Assets/Scenes/Demo/02_TwentyFiveClick/NumberButton.cs
+++ Assets/Scenes/Demo/02_TwentyFiveClick/NumberButton.cs
@@ -1,8 +1,28 @@
+using TMPro;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes.Demo._02_TwentyFiveClick
-{
+{ 
     public class NumberButton : MonoBehaviour
     {
+        [SerializeField] Button _button;
+        [SerializeField] TextMeshProUGUI _numberText;
+        private int _number;
+        
+        void Start()
+        {
+            _button.OnClickAsObservable().Subscribe(_ =>
+            {
+                Debug.Log(_number);
+            });
+        }
+        
+        public void SetNumber(int number)
+        {
+            _number = number;
+            _numberText.text = number.ToString();
+        }
     }
 }
