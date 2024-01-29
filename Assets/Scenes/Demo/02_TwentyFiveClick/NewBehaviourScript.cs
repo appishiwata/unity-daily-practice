a69457922f3e7713a4659113c98e3a8ffe6956df
appishiwata <app.ishiwata@gmail.com>
2024-01-29 09:33:12 +0900

    25早押しゲーム 作成

---
+++ Assets/Scenes/Demo/02_TwentyFiveClick/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Scenes.Demo._02_TwentyFiveClick
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
